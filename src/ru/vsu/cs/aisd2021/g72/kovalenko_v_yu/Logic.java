fbd43151fcd0d6b68ff975222d12fce7a2e74b88
Kovalenko Vladimir <vova06092002@mail.ru>
2021-03-31 23:29:19 +0300

    Task_3, added class LOggic

--- src/ru/vsu/cs/aisd2021/g72/kovalenko_v_yu/Logic.java
+++ src/ru/vsu/cs/aisd2021/g72/kovalenko_v_yu/Logic.java
@@ -1,13 +1,37 @@
 package ru.vsu.cs.aisd2021.g72.kovalenko_v_yu;
 
 public class Logic {
+    private static  int check(MyStack<Queen> queens, Queen cur) throws Exception {
+        MyStack<Queen> auxiliaryStack  = new MyStack<>();
+        while(queens.peek()!=null){
+            auxiliaryStack.push(queens.pop());
+            if(auxiliaryStack.peek().getX()== cur.getX()
+                    && auxiliaryStack.peek().getY()==cur.getY()
+                    && Math.abs(auxiliaryStack.peek().getX()- cur.getX())==Math.abs(auxiliaryStack.peek().getY()- cur.getY())){
+                continue;
+            }else {
+                break;
+            }
+        }
 
-    public static void permutation(){
+return 1;
+    }
+
+    public static void permutation() throws Exception {
    MyStack<Queen> queens = new MyStack<>();
    int i = 0;
-   while (i<9){
+   while (i<8) {
+       Queen current = new Queen(i, 0);
+       for (int j = 0; j < 8; j++) {
+           current.setY(j);
+           if (check(queens, current) > 0) {
+               break;
+           }
 
+           }
+
+       }
    }
 
-    }
+
 }
