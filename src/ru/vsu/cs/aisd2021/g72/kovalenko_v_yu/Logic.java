162f91bbadae66344418669c9e72ea7fcc236e6c
Kovalenko Vladimir <vova06092002@mail.ru>
2021-04-01 00:48:52 +0300

    Добавлена расстановка 8 вариантов, относительно первой поставленной ферзи

--- src/ru/vsu/cs/aisd2021/g72/kovalenko_v_yu/Logic.java
+++ src/ru/vsu/cs/aisd2021/g72/kovalenko_v_yu/Logic.java
@@ -1,37 +1,59 @@
 package ru.vsu.cs.aisd2021.g72.kovalenko_v_yu;
 
 public class Logic {
-    private static  int check(MyStack<Queen> queens, Queen cur) throws Exception {
-        MyStack<Queen> auxiliaryStack  = new MyStack<>();
-        while(queens.peek()!=null){
+    private static boolean check(MyStack<Queen> queens, Queen cur, int count) throws Exception {
+        int counts = count;
+        MyStack<Queen> auxiliaryStack = new MyStack<>();
+        boolean result = true;
+        while (counts != 0 &&queens.peek() != null) {
+            counts --;
             auxiliaryStack.push(queens.pop());
-            if(auxiliaryStack.peek().getX()== cur.getX()
-                    && auxiliaryStack.peek().getY()==cur.getY()
-                    && Math.abs(auxiliaryStack.peek().getX()- cur.getX())==Math.abs(auxiliaryStack.peek().getY()- cur.getY())){
-                continue;
-            }else {
-                break;
+            if (auxiliaryStack.peek().getX() == cur.getX()
+                    || auxiliaryStack.peek().getY() == cur.getY()
+                    || Math.abs(auxiliaryStack.peek().getX() - cur.getX()) == Math.abs(auxiliaryStack.peek().getY() - cur.getY())) {
+                result = false;
             }
-        }
 
-return 1;
+        }
+        while (counts<count && auxiliaryStack.peek() != null) {
+            queens.push(auxiliaryStack.pop());
+            counts++;
+        }
+        if (result)
+            queens.push(cur);
+        //   System.out.println(result);
+        return result;
     }
 
-    public static void permutation() throws Exception {
-   MyStack<Queen> queens = new MyStack<>();
-   int i = 0;
-   while (i<8) {
-       Queen current = new Queen(i, 0);
-       for (int j = 0; j < 8; j++) {
-           current.setY(j);
-           if (check(queens, current) > 0) {
-               break;
-           }
+    public static void permutation(MyStack<Queen> queens, int k ) throws Exception {
+        int i = 1;
+        boolean can = false;
+        queens.push(new Queen(0,k));
+        Queen current = new Queen(i, 0);
+        while (i < 8) {
+            for (int j = current.getY(); j < 8; j++) {
+                current.setY(j);
+                if (check(queens, current, i)) {
+                    can=true;
+                    break;
+                }else {
+                    can=false;
+                }
+                //  System.out.println(j);
+            }
+            if(can){
 
-           }
+                i++;
+                current = new Queen(i, 0);
 
-       }
-   }
+            }else{
+                current = queens.pop();
+                current.setY(current.getY()+1);
+                i--;
+            }
+
+        }
+    }
 
 
 }
--- src/ru/vsu/cs/aisd2021/g72/kovalenko_v_yu/Main.java
+++ src/ru/vsu/cs/aisd2021/g72/kovalenko_v_yu/Main.java
@@ -2,7 +2,19 @@ package ru.vsu.cs.aisd2021.g72.kovalenko_v_yu;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+
+        MyStack<Queen> queens = new MyStack<>();
+        for(int i = 0; i < 8;i++) {
+            Logic.permutation(queens,i);
+            int j = 0;
+            while (j < 8 && queens.peek() != null) {
+                System.out.println(queens.peek().getX() + " " + queens.pop().getY());
+                j++;
+            }
+            System.out.println("-----------------------------------------");
+
+        }
+
     }
 }
